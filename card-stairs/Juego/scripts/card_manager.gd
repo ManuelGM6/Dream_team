b219880a207bf073d40565f8cc3f7f24607ecc0b
5i3 <5i3@cangallo.escuela>
2026-06-12 10:40:38 -0300

    Funciona el card slot

--- card-stairs/Juego/scripts/card_manager.gd
+++ card-stairs/Juego/scripts/card_manager.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 const COLLISION_MASK_CARD = 1
+const COLLISION_MASK_CARD_SLOT = 2
 
 var screen_size
 var card_being_dragged
@@ -14,7 +15,7 @@ func _process(delta: float) -> void:
 	if card_being_dragged:
 		var mouse_pos = get_global_mouse_position()
 		card_being_dragged.position = Vector2(clamp(mouse_pos.x, 0, screen_size.x), 
-		clamp(mouse_pos.y, 0, screen_size.y))
+			clamp(mouse_pos.y, 0, screen_size.y))
 		
 
 func _input(event):
@@ -33,6 +34,11 @@ func start_drag(card):
 
 func finish_drag():
 	card_being_dragged.scale = Vector2(1.05, 1.05)
+	var card_slot_found = raycast_check_for_card_slot()
+	if card_slot_found and not card_slot_found.card_in_slot:
+		card_being_dragged.position = card_slot_found.position
+		card_being_dragged.get_node("Area2D/CollisionShape2D").disabled = true
+		card_slot_found.card_in_slot = true
 	card_being_dragged = null
 
 func connect_card_signals(card):
@@ -72,6 +78,21 @@ func raycast_check_for_card():
 	
 	if result.size() > 0:
 		return get_card_with_highest_z_index(result)
+	return null
+
+
+func raycast_check_for_card_slot():
+	var space_state = get_world_2d().direct_space_state
+	
+	var parameters = PhysicsPointQueryParameters2D.new()
+	parameters.position = get_global_mouse_position()
+	parameters.collide_with_areas = true
+	parameters.collision_mask = COLLISION_MASK_CARD_SLOT
+	
+	var result = space_state.intersect_point(parameters)
+	
+	if result.size() > 0:
+		return result[0].collider.get_parent()
 	return null
 
 
